47fe2155cecfc9c5f8a57a6e45554b7046256809
Ken Jonathan Pais <81014114+kenjpais@users.noreply.github.com>
2021-04-14 23:49:10 +0530

    Add files via upload

---
+++ 3_Implementation/inc/flight_operations.h
@@ -0,0 +1,19 @@
+#ifndef __FUN_H__
+#define __FUN_H__
+
+/**
+ * @brief Function to find sum
+ * @param[in] Numbers
+ * @result int Result of sum
+ */
+
+int sum(int num1, int num2);
+
+/**
+ * @brief Function to find square
+ * @param[in] Number of which square needs to be found
+ * @result int Result of square of num
+ */
+
+int sqr(int num);
+#endif
