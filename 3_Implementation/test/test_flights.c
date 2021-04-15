16ca5591bf4cdbe8f357ba683b2fbe54d4555c42
Ken Jonathan Pais <81014114+kenjpais@users.noreply.github.com>
2021-04-15 23:33:14 +0530

    Add files via upload

---
+++ 3_Implementation/test/test_flights.c
@@ -0,0 +1,79 @@
+#include "Airlines.h"
+#include "unity.h"
+#include<string.h>
+#include "unity_internals.h"
+
+
+
+/* Required by the unity test framework */
+void setUp()
+{
+}
+/* Required by the unity test framework */
+void tearDown()
+{
+}
+
+
+	struct records
+{
+    int P_ID;
+    char p_no[9];
+    char name[15];
+    int seat_num;
+    char email[15];
+	
+}flow;
+
+
+
+
+
+void test_details(void)
+{
+strcpy(flow.p_no,"1234");
+strcpy(flow.name,"ken");
+strcpy(flow.email,"kenjpais@gmail.com");
+
+
+
+TEST_ASSERT_EQUAL_STRING("1234",stream.passport);
+TEST_ASSERT_EQUAL_STRING("ken",stream.name);
+TEST_ASSERT_EQUAL_STRING("kenjpais@gmail.com",stream.email);
+
+
+
+
+}
+
+void test_display(void)
+{
+strcpy(flow.p_no,"1234");
+strcpy(flow.name,"ken");
+strcpy(flow.email,"kenjpais@gmail.com");
+
+
+TEST_ASSERT_EQUAL_STRING("1234",flow.p_no);
+TEST_ASSERT_EQUAL_STRING("ken",flow.name);
+TEST_ASSERT_EQUAL_STRING("kenjpais@gmail.com",flow.email);
+}
+
+
+
+
+
+int main()
+{
+    /* Initiate the Unity Test Framework */
+    UNITY_BEGIN();
+
+    /* Run Test functions */
+    RUN_TEST(test_details);
+    RUN_TEST(test_display);
+    
+    
+
+    /* Close the Unity Test Framework */
+    return UNITY_END();
+}
+
